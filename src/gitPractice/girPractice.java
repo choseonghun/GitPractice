057df0fb39fffe85eb3e5d77d69fce093f7783af
4-PC\4 <whitenut4876@gmail.com>
2018-01-23 13:39:06 +0900

    Scanner 숫자 입력 입력간 출력 요시!!!

--- src/gitPractice/girPractice.java
+++ src/gitPractice/girPractice.java
@@ -1,7 +1,20 @@
 package gitPractice;
 
+import java.util.Scanner;
+
 public class girPractice {
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		System.out.println("안녕");
+		System.out.println("요시!!");
+		
+		//입력
+		System.out.println("숫자를 입력하세요");
+		int left = sc.nextInt();
+		System.out.println("숫자를 또 입력하세요");
+		int right = sc.nextInt();
+		
+		//출력
+		System.out.println("입력한 숫자"+left+","+right);
 	}
 }
